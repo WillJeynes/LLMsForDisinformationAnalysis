3286df645090d5f9bba0559983290e534bc017e5
William Jeynes <william.jeynes@hotmail.com>
2026-03-25 22:32:41 +0000

    remove context examples

--- agent/nodes/triggerEventSetup.ts
+++ agent/nodes/triggerEventSetup.ts
@@ -7,11 +7,11 @@ export const triggerEventSetup: GraphNode<typeof MessagesState> = async (state) 
   let nc = state?.messages?.at(-1)?.content ?? "" //keep a copy of normalized trigger event. Again two things, womp womp
   
   //Now give in-context examples. hopwfully we can self-teach?
-  let similarityResults = await rankExampleTriggerEvents(state.disinformationTitle)
+  // let similarityResults = await rankExampleTriggerEvents(state.disinformationTitle)
 
-  let messages : BaseMessage[] = similarityResults.map((item) => {
-    return new AIMessage(`- Event: ${item.rawtext} \n\n - Claims and given scores: ${item.cleantext}`)
-  })
+  // let messages : BaseMessage[] = similarityResults.map((item) => {
+  //   return new AIMessage(`- Event: ${item.rawtext} \n\n - Claims and given scores: ${item.cleantext}`)
+  // })
   
-  return { messages: messages, disinformationTitle: state.disinformationTitle, normalizedClaim: nc };
+  return { disinformationTitle: state.disinformationTitle, normalizedClaim: nc };
 };
