36c30a427d18abb1a0eaae660a959b7ea64ef242
William Jeynes <william.jeynes@hotmail.com>
2026-03-31 16:08:28 +0100

    update deps. Install ollama for lang chain. Update model to deepseek

--- agent/nodes/model.ts
+++ agent/nodes/model.ts
@@ -1,22 +1,22 @@
 import { HumanMessage, SystemMessage } from "@langchain/core/messages";
 import { GraphNode } from "@langchain/langgraph";
 import { MessagesState } from "../state";
-import { ChatOpenAI } from "@langchain/openai"
+import { ChatOllama } from "@langchain/ollama";
 import { hydratePrompt } from "../prompts/hydratePrompt";
 
 export function createModelNode(tools: any, promptPath: string): GraphNode<typeof MessagesState> {
     return async (state) => {
         const sysPrompt = await hydratePrompt(promptPath, state);
 
-        const model = new ChatOpenAI({
-            model: "gpt-5-mini"
+        const model = new ChatOllama({
+            model: "deepseek-r1:14b",
+            temperature: 0.7,
         });
+
         const modelWithTools = model.bindTools(Object.values(tools));
 
         const response = await modelWithTools.invoke([
-            new SystemMessage(
-                sysPrompt
-            ),
+            new SystemMessage(sysPrompt),
             ...state.messages,
         ]);
 
